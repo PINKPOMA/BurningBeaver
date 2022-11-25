7063584e0a3be7e72180aef424b39eff8eb5a325
ChoiYuBin <choi82354@gmail.com>
2022-11-26 08:17:37 +0900

    Modify SetTileMap
    벽을 넘는 버그를 해결하였습니다.

--- Assets/Scripts/SetTileMap.cs
+++ Assets/Scripts/SetTileMap.cs
@@ -8,9 +8,22 @@ public class SetTileMap : MonoBehaviour
   [SerializeField]private TileBase fireTileBase;
   [SerializeField]private Vector3Int tilePos;
   [SerializeField]private float spreadDelay;
+  
+  [Header("World edge block")]
+  [SerializeField]private float xMin;
+  [SerializeField]private float yMin;
+  [SerializeField]private float xMax;
+  [SerializeField]private float yMax;
+  
+  [Header("World edge block")]
+  [SerializeField]private float waterXMin;
+  [SerializeField]private float waterYMin;
+  [SerializeField]private float waterXMax;
+  [SerializeField]private float waterYMax;
+
 
   private void Start()
-  {
+  { 
     var newTileMap = Instantiate(fireTileMap, Vector3.zero, Quaternion.identity);
     StartCoroutine(SpreadFrame());
     StartCoroutine(WeightedValue());
@@ -29,15 +42,33 @@ public class SetTileMap : MonoBehaviour
   {
     yield return new WaitForSeconds(spreadDelay);
     if(Random.Range(0,2) == 0)
-      tilePos.x += ReturnAddPos();
+      tilePos.x += ReturnAddPosX();
     else
-      tilePos.y += ReturnAddPos();
+      tilePos.y += ReturnAddPosY();
     Instantiate(fireTileMap, tilePos, Quaternion.identity);
     StartCoroutine(SpreadFrame());
   }
 
-  private int ReturnAddPos()
+  private int ReturnAddPosX()
   {
-    return Random.Range(0, 2) == 0 ? -1 : 1;
+    if (Random.Range(0, 2) == 0)
+    {
+      return tilePos.x - 1 < xMin ? 1 : -1;
+    }
+    else
+    {
+      return tilePos.x + 1 > xMax ? -1 : 1;
+    }
+  }
+  private int ReturnAddPosY()
+  {
+    if (Random.Range(0, 2) == 0)
+    {
+      return tilePos.y - 1 < yMin ? 1 : -1;
+    }
+    else
+    {
+      return tilePos.y + 1 > yMax ?  -1 : 1;
+    }
   }
 }
