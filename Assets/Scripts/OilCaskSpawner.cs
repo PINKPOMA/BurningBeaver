b4f3521ae264b02aee1a0dd82526bee51f225205
anminwoo <minwoo5995@gmail.com>
2022-11-26 21:17:48 +0900

    Modify OilCaskSpawner spawnPos

--- Assets/Scripts/OilCaskSpawner.cs
+++ Assets/Scripts/OilCaskSpawner.cs
@@ -1,56 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using Quaternion = UnityEngine.Quaternion;
 using Random = UnityEngine.Random;
 
 public class OilCaskSpawner : MonoBehaviour
 {
-    [SerializeField]private Vector3 playerPos;
     [SerializeField] private Tilemap oilCaskTilemap;
-    [Header("World edge block")]
-    [SerializeField]private float xMin;
-    [SerializeField]private float yMin;
-    [SerializeField]private float xMax;
-    [SerializeField]private float yMax;
+    [SerializeField] private Vector3Int[] oilCaskSpawnPos;
 
     private void Start()
     {
-        RandomPos();
         StartCoroutine(OilCaskSpawn());
     }
 
     IEnumerator OilCaskSpawn()
     {
-        Instantiate(oilCaskTilemap, playerPos, Quaternion.identity);
+        Instantiate(oilCaskTilemap,oilCaskSpawnPos[Random.Range(0, 15)] , Quaternion.identity);
         yield return new WaitForSeconds(15f);
-        RandomPos();
         StartCoroutine(OilCaskSpawn());
-    }
-    void RandomPos()
-    {
-        playerPos = GameObject.FindWithTag("Player").GetComponent<Transform>().position;
-        if (Random.Range(0, 2) == 0)
-        {
-            playerPos.x = ReturnAddPlayerPos();
-            if (playerPos.x > xMax)
-                playerPos.x = xMax;
-            if (playerPos.x < xMin)
-                playerPos.x = xMin;
-        }
-        else
-        {
-            playerPos.y += ReturnAddPlayerPos();
-            if (playerPos.y > yMax)
-                playerPos.y = yMax;
-            if (playerPos.y < yMin)
-                playerPos.y = yMin;
-        }
-        
-    }
-    float ReturnAddPlayerPos()
-    {
-        return Random.Range(0, 2) == 0 ? Random.Range(-3, -5) : Random.Range(3, 5);
     }
 }
