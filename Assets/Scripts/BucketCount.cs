d80bfac5e2ae0278c1408f2a5ca6af369de0d39e
anminwoo <minwoo5995@gmail.com>
2022-11-26 15:57:16 +0900

    Modify BucketCount

--- Assets/Scripts/BucketCount.cs
+++ Assets/Scripts/BucketCount.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,7 +8,48 @@ using UnityEngine.UI;
 
 public class BucketCount : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI waterBucketCountText;
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite emptyBucketSprite;
+    [SerializeField] private Sprite fillBucketSprite;
+    
+    [SerializeField] private Image[] buckets;
+    private int nextSpendWater;
+    private int nextFillWater;
 
-    public void ChangeWaterBucketCount(int count) => waterBucketCountText.text = "x" + count;
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        nextSpendWater = buckets.Length - 1;
+        nextFillWater = 0;
+    }
+
+    public void SpendWaterBucket()
+    {
+        buckets[nextSpendWater].sprite = emptyBucketSprite;
+        nextFillWater = nextSpendWater;
+        nextSpendWater--;
+    }
+
+    public void FillWaterBucket()
+    {
+        buckets[nextFillWater].sprite = fillBucketSprite;
+        nextSpendWater = nextFillWater;
+        nextFillWater++;
+    }
+
+    public void Init(int waterCapacity)
+    {
+        for (int i = 0; i < buckets.Length; i++)
+        {
+            buckets[i].enabled = false;
+        }
+        for (int i = 0; i < waterCapacity; i++)
+        {
+            buckets[i].enabled = true;
+        }
+    }
 }
